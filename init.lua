39b0ba045e9f4f50a879141ffb03c3d3ec269eb4
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-08-18 10:59:57 -0700

    prettier but its hacky

--- init.lua
+++ init.lua
@@ -72,6 +72,8 @@ require('lazy').setup({
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
+  'jose-elias-alvarez/null-ls.nvim',
+  'MunifTanjim/prettier.nvim',
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -88,7 +90,6 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -218,6 +219,9 @@ require('lazy').setup({
 
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+-- [[ Configure LuaSnip ]]
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
@@ -237,6 +241,60 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+-- [[ Configure Prettier ]]
+local null_ls = require("null-ls")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
 })
 
 -- [[ Configure Telescope ]]
@@ -423,6 +481,7 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
+  eslint = { filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'json' } },
   html = { filetypes = { 'html', 'twig', 'hbs', "gohtmltmpl" } },
 
   lua_ls = {
---
+++ lua/custom/plugins/vim-prisma.lua
@@ -0,0 +1,3 @@
+return {
+	"prisma/vim-prisma"
+}
