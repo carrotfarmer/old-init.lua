cb9f5e95300dfd68ea2dfa0f72429c997d57597b
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-08-24 14:51:13 -0700

    fix copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept(\"<CR>\")", { expr = true, silent = true })
+	end
 }
